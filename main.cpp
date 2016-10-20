dc3b71eb87c9f1a5aadf3355d62255309ada326b
Jian Wei <jianwei@jianwei-deb8-64>
2016-10-20 13:43:40 -0400

    first edit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using std::vector;
+using std::pair;
+
+class solution {
+  public:
+    int numOfArithSlices(const vector<int> & arr) {
+        size_t begin = 0, end;
+        
+    }
+
+  private:
+    size_t findArithSubset(const vector<int> & arr, size_t start) {
+        
+    }
+
+    bool isArith(const vector<int> & arr, size_t begin, size_t end) {
+        if (end-begin < 2)
+            return false;
+        else {
+            
+        }
+    }
+};
+
+int main() {
+    vector<int> nums{1,2,3,4};
+    
+}
